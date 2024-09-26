92fadd64eba136a5c5f233112e9589ed92e4308a
mira-yoon <amorfati823@gmail.com>
2024-09-26 14:35:34 +0900

    React.memo와 컴포넌트 렌더링 최적화

--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -25,4 +25,18 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default memo(TodoItem);
+// 고차 컴포넌트(HOC)
+// memo 함수 안에 두 번째 인수로 콜백함수를 인자로 전달하면 , 콜백함수의 매개변수 prevProps, nextProps를 전달해줘서 이 함수의 반환값에 따라서 props가 바뀌었는지 안바뀌었는지 판단한다.
+export default memo(TodoItem, (prevProps, nextProps) => {
+  // 콜백함수가 true 반환 → props가 바뀌지 않음 → 리렌더링 X
+  // 콜백함수가 false 반환 → props가 바뀜 → 리렌더링 O
+
+  // id, isDone, content, date가 바뀌었을 때만 리렌더링 되게 해라
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+
+  // 바뀐 값이 없다면 리렌더링 하지 마라
+  return true;
+});
