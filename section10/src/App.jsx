ceddc0aaaf8748da6925f76fa519dd8b61470d7e
mira-yoon <amorfati823@gmail.com>
2024-09-26 16:28:34 +0900

    useCallback과 함수 재생성 방지

--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useRef, useReducer } from "react";
+import { useState, useRef, useReducer, useCallback } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
@@ -58,19 +58,39 @@ function App() {
     });
   };
 
-  const onUpdate = (targetId) => {
+  // const onUpdate = (targetId) => {
+  //   dispatch({
+  //     type: "UPDATE",
+  //     targetId: targetId,
+  //   });
+  // };
+
+  const onUpdate = useCallback((targetId) => {
     dispatch({
       type: "UPDATE",
       targetId: targetId,
     });
-  };
+  }, []);
 
-  const onDelete = (targetId) => {
+  // const onDelete = (targetId) => {
+  //   dispatch({
+  //     type: "DELETE",
+  //     targetId: targetId,
+  //   });
+  // };
+
+  const onDelete = useCallback((targetId) => {
     dispatch({
       type: "DELETE",
       targetId: targetId,
     });
-  };
+  }, []);
+
+  // 첫 번째 인수: 최적화하고싶은 함수, 두 번째 인수: deps
+  // useCallback은 두 번째 인수로 넣은 콜백함수를 그대로 생성해서 반환한다.
+  // 이렇게 생성되는 함수는 deps가 변경되었을 때만 최적화를 해준다.(함수를 메모이제이션해 주는 것이다)
+  // deps가 빈 배열일 경우에는 컴포넌트가 최초로 렌더링되었을 때만 함수를 생성한다.
+  // const func = useCallback(() => {}, []);
 
   return (
     <div className="App">
