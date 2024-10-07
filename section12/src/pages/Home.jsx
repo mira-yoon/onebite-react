818fb560732894ee7fc041acda3968850bd44a63
mira-yoon <amorfati823@gmail.com>
2024-10-07 17:54:51 +0900

    페이지 라우팅: 동적 경로

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,4 +1,11 @@
+import { useSearchParams } from "react-router-dom";
+
+// http://localhost:5173/?value=hello로 들어왔을 때.
 const Home = () => {
+  const [params, setParams] = useSearchParams();
+  // params라는 변수에는 queryString으로 전달한 변수와 값이 들어온다.
+  // setParams에는 특정 queryString의 값을 변경할 수 있는 함수가 들어오게 된다.
+  console.log(params.get("value")); // hello
   return <div>Home</div>;
 };
 
