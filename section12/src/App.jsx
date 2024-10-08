5303a0457e03ce9cd632695ae4e66ce2125c7fb3
mira-yoon <amorfati823@gmail.com>
2024-10-08 15:28:47 +0900

    폰트, 이미지 레이아웃 설정하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,10 +4,11 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
+import { getEmotionImage } from "./util/get-emotion-image";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/Diary" : 일기를 상세히 조회하는 Diary 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
   const nav = useNavigate();
 
@@ -16,6 +17,13 @@ function App() {
   };
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} alt="" />
+        <img src={getEmotionImage(2)} alt="" />
+        <img src={getEmotionImage(3)} alt="" />
+        <img src={getEmotionImage(4)} alt="" />
+        <img src={getEmotionImage(5)} alt="" />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
