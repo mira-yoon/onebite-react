7ba056165f5a1dc2242f881ed65e0e54ce08569e
mira-yoon <amorfati823@gmail.com>
2024-10-08 17:50:42 +0900

    Edit 컴포넌트 추가

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -3,15 +3,12 @@ import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Edit from "./pages/Edit";
 import Notfound from "./pages/Notfound";
+
 import Button from "./components/Button";
 import Header from "./components/Header";
 
-import { getEmotionImage } from "./util/get-emotion-image";
-
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
   const nav = useNavigate();
 
@@ -25,33 +22,11 @@ function App() {
         leftChild={<Button text={"Left"} />}
         rightChild={<Button text={"Right"} />}
       />
-      <Button
-        text={"123"}
-        type={"DEFAULT"}
-        onClick={() => {
-          console.log(123);
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log(123);
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log(123);
-        }}
-      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
