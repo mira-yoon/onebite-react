8a68b95e50367c1b2e1307590e79e169406c86f7
mira-yoon <amorfati823@gmail.com>
2024-10-10 11:47:04 +0900

    일기 관리 기능 구현하기1

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useReducer } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
@@ -9,7 +10,27 @@ import Notfound from "./pages/Notfound";
 import Button from "./components/Button";
 import Header from "./components/Header";
 
+const mocData = [
+  {
+    id: 1,
+    createDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, mocData);
   return (
     <>
       <Header
