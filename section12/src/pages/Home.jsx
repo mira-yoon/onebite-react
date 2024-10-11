455052587e8be733063be4fe1dde409f603aa114
mira-yoon <amorfati823@gmail.com>
2024-10-11 17:57:29 +0900

    Home 페이지 구현하기 2. 기능 작업중

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,14 +1,59 @@
+import { useState, useContext } from "react";
+import { DiaryStateContext } from "../App";
+
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 
+const getMonthlyData = (pivotDate, data) => {
+  // 해당 연, 월의 1일 0시 0분 0초
+  const beginTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth(),
+    1,
+    0,
+    0,
+    0
+  ).getTime();
+
+  // 해당 연, 월의 마지막 날.
+  // 다음 달로 설정하고, "일"을 0일로 설정한다. 그러면 이전 달의 마지막 날로 설정된다.
+  const endTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1,
+    0,
+    23,
+    59,
+    59
+  ).getTime();
+
+  // createdDate가 beginTime보다는 나중이고, endTime보다는 이전이면 이번달에 작성된 일기가 맞으니 필터링해서 새 배열 반환
+  return data.filter(
+    (item) => beginTime <= item.createdDate && item.createdDate <= endTime
+  );
+};
+
 const Home = () => {
+  const data = useContext(DiaryStateContext);
+  console.log(data);
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const monthlyData = getMonthlyData(pivotDate, data);
+  console.log(monthlyData);
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
   return (
     <div>
       <Header
-        title={"2024년 2월"}
-        leftChild={<Button text={"<"} />}
-        rightChild={<Button text={">"} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
       />
       <DiaryList />
     </div>
