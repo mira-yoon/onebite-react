7576d405fe09bdaba4f85d2de466a108ded81163
mira-yoon <amorfati823@gmail.com>
2024-10-14 17:24:14 +0900

    New 페이지 구현하기 1. UI

---
+++ section12/src/components/EmotionItem.jsx
@@ -0,0 +1,17 @@
+import "./EmotionItem.css";
+import { getEmotionImage } from "../util/get-emotion-image";
+
+const EmotionItem = ({ emotionId, emotionName, isSelected }) => {
+  return (
+    <div
+      className={`EmotionItem ${
+        isSelected ? `EmotionItem_on${emotionId}` : ""
+      }`}
+    >
+      <img className="emotion_img" src={getEmotionImage(emotionId)} alt="" />
+      <div className="emotion_name">{emotionName}</div>
+    </div>
+  );
+};
+
+export default EmotionItem;
